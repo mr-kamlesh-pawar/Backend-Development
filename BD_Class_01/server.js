8b0b9143ebc82bcd681fe51477ed54bc7f586f83
Kamlesh Pawar <kamleshpawar5278@gmail.com>
2024-10-19 01:01:23 +0530

    updated

---
+++ BD_Class_01/server.js
@@ -0,0 +1,45 @@
+// 1. create folder
+// 2. command- npm init -y
+// 3. command- npm install express
+// 4. new file server.js
+
+const express= require('express');
+const app= express();
+
+//used to parse req.body in express -> PUT or POST
+const bodyParser= require('body-parser');
+app.use(bodyParser.json());  //specifically parse json data add it on to the request.body object
+
+app.listen(3000, ()=>{
+    console.log("Server started at port no 3000");
+    
+})
+
+app.get('/', (req,res)=>{
+    res.send("hello ji kya haal chal")
+})
+
+app.post('/api/cars', (req,res)=>{
+    const {name, brand}=req.body;
+    console.log(name);
+    console.log(brand);
+    res.send("Car data submitted Sucessfully");   
+    })
+
+    //db connection
+
+    const mongoose = require('mongoose');
+    mongoose.connect('mongodb://localhost:27017/MyDatabase', {
+        useNewurlParser:true,
+        useUnifiedTopology:true
+    })
+    .then(()=>{
+        console.log("DB connected");
+    })
+
+    .catch((error)=>{
+        console.log("Bhai error aa gya",error);
+        
+
+    });
+    
